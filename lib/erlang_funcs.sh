51f7dd81a6af57712e0ee1e8a4c6e809d61c4073
John Maxwell <jgwmaxwell@gmail.com>
2018-09-09 16:24:35 +0100

    Update erlang_funcs.sh

--- lib/erlang_funcs.sh
+++ lib/erlang_funcs.sh
@@ -14,7 +14,7 @@ function download_erlang() {
     erlang_changed=true
 
     output_section "Fetching Erlang ${erlang_version}"
-    output_section "url: '${erlang_package_url}'"
+    echo "url: '${erlang_package_url}'"
     curl -s ${erlang_package_url} -o ${cache_path}/$(erlang_tarball) || exit 1
   else
     output_section "Using cached Erlang ${erlang_version}"
